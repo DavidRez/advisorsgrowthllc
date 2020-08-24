fa13a9623908ddd82d9601abbc4c13e6de6be541
Matt Sprague <me@spragala.com>
2020-08-24 15:32:39 -0700

    updates(fonts): adds comments for adding font-display

--- resources/fonts.js
+++ resources/fonts.js
@@ -1,5 +1,8 @@
+// To add the font-display property to Google fonts add `&display=swap` to the end of the LAST font-family
+// For example: families: [ 'Playfair+Display', 'Raleway:500,700&display=swap' ]
+
 export default {
   google: {
-    families: [ 'Open+Sans:400,700' ]
+    families: [ 'Open+Sans:400,700&display=swap' ]
   }
 }
