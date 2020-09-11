fb12895d0769327a88a05f2e77c5213a43c6175d
luketevers <luketevers@gmail.com>
2020-09-10 20:56:32 -0700

    feat(global-components): creates demo for new data calls

--- components/navigation/navigation.vue
+++ components/navigation/navigation.vue
@@ -1,18 +1,22 @@
 <template lang='pug' src='./navigation.pug'></template>
 
 <script>
+import { postFetchHelper } from '../../resources/utils'
 import router from '~/router/index'
 
 export default {
+  data: () => ({
+    content: null
+  }),
   computed: {
-    // props () {
-    //   return this.$store.state.globalData
-    // },
     links () {
       return router.filter((link) => {
         return link.navigation
       })
     }
+  },
+  async fetch () {
+    this.content = await postFetchHelper('navigation', 'globalData')
   }
 }
 </script>
