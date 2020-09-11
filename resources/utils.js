f643178d335f4f059c028d3476be68e2d60075b2
luketevers <luketevers@gmail.com>
2020-09-10 20:58:36 -0700

    updates(utils.js): adds post fetch helper

--- resources/utils.js
+++ resources/utils.js
@@ -1,9 +1,39 @@
-export const setMeta = (props) => {
+import axios from 'axios'
+import { api } from './api'
+
+export const postFetchHelper = async (slug, customPostType = 'pages') => {
+  try {
+    const response = await axios.get(
+      `${api}/wp/v2/${customPostType}?slug=${slug}`
+    )
+
+    const data = {
+      _aioseo_title: response.data[0]._aioseop_title,
+      description: response.data[0]._aioseop_description,
+      keywords: response.data[0]._aioseop_keywords,
+      title: response.data[0].title.rendered,
+      ...response.data[0].acf
+    }
+    return { ...data }
+  } catch (e) {
+    console.error(`${slug} page: ${e}`)
+  }
+}
+
+export const setMeta = (content) => {
   return {
-    title: props.aioseop_title ? props.aioseop_title : props.title,
+    title: content._aioseo_title ? content._aioseo_title : content.title,
     meta: [
-      props.aioseop_description && { hid: 'description', name: 'description', content: props.aioseop_description },
-      props.aioseop_keywords && { hid: 'keywords', name: 'keywords', content: props.aioseop_keywords }
+      content.description && {
+        hid: 'description',
+        name: 'description',
+        content: content.description
+      },
+      content.keywords && {
+        hid: 'keywords',
+        name: 'keywords',
+        content: content.keywords
+      }
     ]
   }
 }
