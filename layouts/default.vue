c028c7657f83336cc8cedeba2d13eac5655aad23
Matt Sprague <me@spragala.com>
2021-12-07 15:40:04 -0800

    updates(layout): creates main content skip link, adds to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,21 @@
 <template src='./default.pug' lang='pug'></template>
 
 <script>
+import SkipLink from '~/components/base/base-skip-link'
 import TheFooter from '~/components/footer/footer'
 import TheNavigation from '~/components/navigation/navigation'
 
 export default {
   components: {
+    SkipLink,
     TheFooter,
     TheNavigation
   }
 }
 </script>
+
+<style>
+  main:focus {
+    outline: none;
+  }
+</style>
