6b6ca4bd2236d0fba32db8419e0e7d402192f9d1
Matt Sprague <me@spragala.com>
2021-12-08 13:38:01 -0800

    updates(mixins) adds mixin to trap focus in a modal

--- resources/mixins.js
+++ resources/mixins.js
@@ -7,3 +7,37 @@ export const onScroll = {
     }
   }
 }
+
+export const trapFocus = {
+  methods: {
+    $_trapFocus (element) {
+      const focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])')
+      const firstFocusableEl = focusableEls[0]
+      const lastFocusableEL = focusableEls[focusableEls.length - 1]
+
+      element.style.outline = 'transparent'
+
+      element.addEventListener('keydown', function (e) {
+        const isTabPressed = (e.key === 'Tab' || e.keyCode === 9)
+
+        if (!isTabPressed) {
+          return
+        }
+
+        /* eslint-disable */
+        if (e.shiftKey) {
+          if (document.activeElement === firstFocusableEl) {
+              e.preventDefault()
+            lastFocusableEL.focus()
+          }
+        } else {
+          if (document.activeElement === lastFocusableEL) {
+            e.preventDefault()
+            firstFocusableEl.focus()
+          }
+        }
+      })
+      element.focus()
+    }
+  }
+}
