607cb660612de107e96911a46479a1bbc8404be6
David Ramirez <davidraviles@gmail.com>
2022-08-23 09:36:22 -0700

    updates(*): homepage setup.

---
+++ components/custom/custom-home-report-feature/custom-home-report-feature.vue
@@ -0,0 +1,45 @@
+<template lang="pug" src="./custom-home-report-feature.pug"></template>
+
+<script>
+import { fadeIn } from '~/resources/mixins'
+
+export default {
+  mixins: [fadeIn],
+  props: {
+    props: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  mounted () {
+    if (this.$store.state.siteLoaded) {
+      this.handleAnimation()
+    } else {
+      this.$store.watch(
+        state => this.$store.state.siteLoaded,
+        (newVal) => {
+          if (newVal) {
+            this.handleAnimation()
+          }
+        }
+      )
+    }
+  },
+  methods: {
+    handleAnimation () {
+      if (this.props.image) {
+        this.$_fadeIn(this.$refs.image.$el, 24, 0, 'top+=58', 0, 1.2)
+      }
+      if (this.props.title || this.props.body) {
+        this.$_fadeIn(this.$refs.text, 24, 0, 'top+=58', 0, 1.2)
+      }
+      if (this.props.links) {
+        this.$_fadeIn(this.$refs.links, 24, 0, 'top+=58', 0, 1.2)
+      }
+    }
+  }
+}
+
+</script>
+
+<style lang="sass" src="./custom-home-report-feature.sass"></style>
