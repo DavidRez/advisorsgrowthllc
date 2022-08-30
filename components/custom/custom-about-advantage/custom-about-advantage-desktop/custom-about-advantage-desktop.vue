4deeada64c47bb94f3893b29227200c275bd6871
David Ramirez <davidraviles@gmail.com>
2022-08-30 11:59:29 -0700

    updates(*): AGS Advantage interactive component setup.

---
+++ components/custom/custom-about-advantage/custom-about-advantage-desktop/custom-about-advantage-desktop.vue
@@ -0,0 +1,55 @@
+<template lang='pug' src='./custom-about-advantage-desktop.pug'></template>
+
+<script>
+import { fadeIn } from '~/resources/mixins'
+
+export default {
+  mixins: [fadeIn],
+  props: {
+    props: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  mounted () {
+    if (this.$store.state.siteLoaded) {
+      this.handleAnimation()
+    } else {
+      this.$store.watch(
+        state => this.$store.state.siteLoaded,
+        (newVal) => {
+          if (newVal) {
+            this.handleAnimation()
+          }
+        }
+      )
+    }
+  },
+  methods: {
+    handleAnimation () {
+      this.$CustomEase.create('customEaseOut', '0.23, 1, 0.32, 1')
+      this.$nextTick(() => {
+        this.$refs.circles.forEach((circle, i) => {
+          this.$CustomEase.create('customEaseOut', '0.23, 1, 0.32, 1')
+          const circletl = this.$gsap.timeline({
+            scrollTrigger: {
+              trigger: circle,
+              start: 'top+=48 bottom'
+            }
+          })
+          const delay = 0.1 + (0.1 * i)
+          circletl.from(circle, {
+            opacity: 0,
+            scale: 0,
+            delay,
+            duration: 0.8,
+            ease: 'customEaseOut'
+          })
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style lang='sass' src='./custom-about-advantage-desktop.sass'></style>
