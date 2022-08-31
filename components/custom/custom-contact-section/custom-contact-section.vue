2ce399e07ced533609a21807307e833c24329320
David Ramirez <davidraviles@gmail.com>
2022-08-31 11:26:38 -0700

    updates(custom-contact-section): fixed min-height.

--- components/custom/custom-contact-section/custom-contact-section.vue
+++ components/custom/custom-contact-section/custom-contact-section.vue
@@ -2,10 +2,10 @@
 
 <script>
 import { setData } from '~/resources/utils'
-import { fadeIn } from '~/resources/mixins'
+import { fadeIn, debounce } from '~/resources/mixins'
 
 export default {
-  mixins: [fadeIn],
+  mixins: [fadeIn, debounce],
   props: {
     props: {
       type: Object,
@@ -13,33 +13,48 @@ export default {
     }
   },
   data: () => ({
-    global: null
+    global: null,
+    elemMinHeight: 0,
+    windowWidth: 0
   }),
   async fetch () {
     this.global = await setData('global', 'globalData')
   },
   mounted () {
-    if (this.$store.state.siteLoaded) {
+    this.handleResize()
+    window.addEventListener('resize', this.debounceFunc)
+    this.$nextTick(() => {
+      if (!this.$store.state.siteLoaded) {
+        this.$store.dispatch('VIEW_SITE', true)
+      }
       this.handleAnimation()
-    } else {
-      this.$store.watch(
-        state => this.$store.state.siteLoaded,
-        (newVal) => {
-          if (newVal) {
-            this.handleAnimation()
-          }
-        }
-      )
-    }
+    })
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.debounceFunc)
   },
   methods: {
+    debounceFunc () {
+      this.debounce(this.handleResize, null, 300)
+    },
+    handleResize () {
+      this.windowWidth = window.innerWidth
+      this.setMinHeight()
+    },
+    setMinHeight () {
+      const footerHeight = document.querySelector('footer').clientHeight
+      const navHeight = document.querySelector('.navigation__top').clientHeight
+      this.elemMinHeight = `${window.innerHeight - (footerHeight + navHeight)}px`
+    },
     handleAnimation () {
-      if (this.props.header) {
-        this.$_fadeIn(this.$refs.header, 24, 0, 'top+=58', 0, 1.2)
-      }
-      if (this.props.subheader) {
-        this.$_fadeIn(this.$refs.subheader, 24, 0, 'top+=58', 0, 1.2)
-      }
+      this.$nextTick(() => {
+        if (this.props.header) {
+          this.$_fadeIn(this.$refs.header, 24, 0, 'top+=58', 0, 1.2)
+        }
+        if (this.props.subheader) {
+          this.$_fadeIn(this.$refs.subheader, 24, 0, 'top+=58', 0, 1.2)
+        }
+      })
     }
   }
 }
