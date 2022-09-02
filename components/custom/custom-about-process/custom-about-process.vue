fa3f971c9e5c86315ebd58fe6fa7184ad157b2ad
David Ramirez <davidraviles@gmail.com>
2022-09-02 09:33:02 -0700

    updates(custom-about-process): modified animation.

--- components/custom/custom-about-process/custom-about-process.vue
+++ components/custom/custom-about-process/custom-about-process.vue
@@ -34,26 +34,10 @@ export default {
     },
     handleAnimation () {
       this.$nextTick(() => {
-        this.$_fadeIn(this.$refs.title, 24, 0, 'top+=58', 1, 1.2)
-        this.$_fadeIn(this.$refs.body, 24, 0, 'top+=58', 1, 1.2)
-        this.$CustomEase.create('customEaseOut', '0.23, 1, 0.32, 1')
-        this.$refs.tabs.forEach((tab, i) => {
-          this.$CustomEase.create('customEaseOut', '0.23, 1, 0.32, 1')
-          const tabtl = this.$gsap.timeline({
-            scrollTrigger: {
-              trigger: tab,
-              start: 'center bottom'
-            }
-          })
-          const delay = 0.1 + (0.1 * i)
-          tabtl.from(tab, {
-            opacity: 0,
-            x: -32,
-            delay,
-            duration: 0.8,
-            ease: 'customEaseOut'
-          })
-        })
+        this.$_fadeIn(this.$refs.box, 24, 0, 'top+=58', 0, 1.2)
+        this.$_fadeIn(this.$refs.tabs, 0, 60, 'top+=58', 0, 2)
+        this.$_fadeIn(this.$refs.title, 24, 0, 'top+=58', 0.5, 1.2)
+        this.$_fadeIn(this.$refs.body, 24, 0, 'top+=58', 0.5, 1.2)
       })
     }
   }
