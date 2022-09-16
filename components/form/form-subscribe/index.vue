0b081657128b41b2be64dda6be3c807a48f6ee86
David Ramirez <davidraviles@gmail.com>
2022-09-16 15:16:02 -0700

    updates(form-subscription): hid submit

--- components/form/form-subscribe/index.vue
+++ components/form/form-subscribe/index.vue
@@ -37,31 +37,31 @@ export default {
           console.error(e)
         })
     },
-    async onSubmit () {
+    onSubmit () {
       try {
-        const portalId = '4784979'
-        const formGuid = '2c12ed74-4d36-416d-a86e-a813640f9f14'
-        const config = {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-        const postURL = `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formGuid}/`
-        this.formSubmitted = true
-        await this.$axios.post(postURL, {
-          fields: [
-            {
-              name: 'email',
-              value: this.clientEmail
-            }
-          ],
-          context: {
-            pageUri: 'https://www.advisorgrowthllc.com/subscribe',
-            pageName: 'Subscribe Page'
-          }
-        },
-        config
-        )
+        // const portalId = '4784979'
+        // const formGuid = '2c12ed74-4d36-416d-a86e-a813640f9f14'
+        // const config = {
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   }
+        // }
+        // const postURL = `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formGuid}/`
+        // this.formSubmitted = true
+        // await this.$axios.post(postURL, {
+        //   fields: [
+        //     {
+        //       name: 'email',
+        //       value: this.clientEmail
+        //     }
+        //   ],
+        //   context: {
+        //     pageUri: 'https://www.advisorgrowthllc.com/subscribe',
+        //     pageName: 'Subscribe Page'
+        //   }
+        // },
+        // config
+        // )
 
         this.formSuccess = true
 
