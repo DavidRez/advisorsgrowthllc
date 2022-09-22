92b231041489fb8160de7a5d75c4d233903666af
David Ramirez <davidraviles@gmail.com>
2022-09-22 12:08:29 -0700

    updates(team): imported components.

--- pages/team/_team.vue
+++ pages/team/_team.vue
@@ -2,9 +2,13 @@
 
 <script>
 import { setData, setMeta } from '~/resources/utils'
+import HeroPages from '~/components/hero/hero-pages/hero-pages'
+import CustomTeamMember from '~/components/custom/custom-team-member/custom-team-member'
 
 export default {
   components: {
+    HeroPages,
+    CustomTeamMember
   },
   async asyncData ({ params }) {
     const data = await setData(params.team, 'team')
