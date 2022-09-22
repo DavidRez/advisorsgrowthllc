326ab085cdad94cb052e9d335cebef0d8ff33326
Bryant Fukushima <bmfukushima94@gmail.com>
2022-09-22 13:28:37 -0700

    updates(thought-leadership): order of thought leadership categories/default page

--- pages/thought-leadership/index.vue
+++ pages/thought-leadership/index.vue
@@ -31,16 +31,25 @@ export default {
   async asyncData ({query}) {
     try {
       // Get All Blog Post Categories
-      // const categoriesResponse = await axios.get(`${api}/wp/v2/categories?per_page=100`)
-      // const categories = categoriesResponse.data.reduce(
-      //   (acc, item) => [
-      //     ...acc,
-      //     { id: item.id, name: item.name }
-      //   ],
-      //   []
-      // )
+      const categoriesResponse = await axios.get(`${api}/wp/v2/the-wire-categories?per_page=100`)
+      const categories = categoriesResponse.data.reduce(
+        (acc, item) => [
+          ...acc,
+          { id: item.id, name: item.name }
+        ],
+        []
+      )
+      const categoriesArchiveResponse = await axios.get(`${api}/wp/v2/the-wire-archive-categories?per_page=100`)
+      const categoriesArchive = categoriesArchiveResponse.data.reduce(
+        (acc, item) => [
+          ...acc,
+          { id: item.id, name: item.name }
+        ],
+        []
+      )
       // Get All Blog Posts
-      const apiStr = query.page ? `${api}/wp/v2/posts?per_page=12&page=${query.page}` : `${api}/wp/v2/posts?per_page=12`
+      const archiveID = categoriesArchive.find(cat => cat.name === 'Archive').id
+      const apiStr = query.page ? `${api}/wp/v2/the-wire?per_page=12&page=${query.page}&the-wire-archive-categories_exclude=${archiveID}` : `${api}/wp/v2/the-wire?per_page=12&the-wire-archive-categories_exclude=${archiveID}`
       const response = await axios.get(apiStr)
       const totalPages = +response.headers['x-wp-totalpages']
       const blogs = response.data.reduce(
@@ -51,9 +60,9 @@ export default {
         []
       )
       const props = await setData('thought-leadership')
-      return { blogs, props, totalPages }
+      return { blogs, categories, props, totalPages, archiveID }
     } catch (e) {
-      console.error('AGS Studies: ' + e)
+      console.error('The Wire: ' + e)
     }
   },
   head () {
